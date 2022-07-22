7b3ff441c0723864b3a3fff7537ac892cb503f72
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-22 19:45:14 -0300

    basic configuration

---
+++ config.py
@@ -0,0 +1,16 @@
+from utilities import swallow
+
+
+main = None
+
+auto_fullscreen = True
+bring_front_click = False
+cursor_wrap = False
+
+dgrous_key_binder = None
+dgroups_app_rules = []
+
+follow_mouse_focus = True
+focus_on_window_activation = "focus"
+
+wmname = "LG3D"
