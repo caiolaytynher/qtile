e3b12758561ca97ee002b19f2c8380a1094323c6
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-23 01:11:28 -0300

    created hooks component

---
+++ components/hooks.py
@@ -0,0 +1,66 @@
+import subprocess
+import psutil
+
+from libqtile import hook
+from libqtile.command import lazy
+
+from components import HOME
+from components.groups import floating_types
+
+
+@lazy.function
+def window_to_prev_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
+
+
+@lazy.function
+def window_to_next_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+
+
+@hook.subscribe.startup_once
+def start_once():
+    subprocess.call([f"{HOME}/.config/qtile/scripts/autostart.sh"])
+
+
+@hook.subscribe.startup
+def start_always():
+    # Set the cursor to something sane in X
+    subprocess.Popen(["xsetroot", "-cursor_name", "left_ptr"])
+
+
+@hook.subscribe.client_new
+def set_floating(window):
+    if (
+        window.window.get_wm_transient_for()
+        or window.window.get_wm_type() in floating_types
+    ):
+        window.floating = True
+
+
+@hook.subscribe.client_new
+def _swallow(window):
+    pid = window.window.get_net_wm_pid()
+    ppid = psutil.Process(pid).ppid()
+    cpids = {
+        c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()
+    }
+    for i in range(5):
+        if not ppid:
+            return
+        if ppid in cpids:
+            parent = window.qtile.windows_map.get(cpids[ppid])
+            parent.minimized = True
+            window.parent = parent
+            return
+        ppid = psutil.Process(ppid).ppid()
+
+
+@hook.subscribe.client_killed
+def _unswallow(window):
+    if hasattr(window, "parent"):
+        window.parent.minimized = False
