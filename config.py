a960b5fd44a154e78e6ee64d4020621dc20edf3e
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-23 02:22:47 -0300

    added prompt value

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-from components import hooks
+from components import hooks, USER, HOSTNAME
 from components.groups import floating_layout, groups, layouts
 from components.keymaps import keys, mouse
 from components.panel import screens, widget_defaults
 
 main = None
+
+prompt = f"{USER}@{HOSTNAME}: "
 
 auto_fullscreen = True
 bring_front_click = False
